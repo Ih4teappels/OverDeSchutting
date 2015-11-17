9a9ab979e3e368143ceeb8b712f2fa91ddb27849
Ih4teappels <kev10@live.nl>
2015-11-17 12:38:23 +0100

    update 3.0

--- views/head.php
+++ views/head.php
@@ -1,11 +1,7 @@
 <html>
 <head>
-<<<<<<< HEAD
-	<link rel="stylesheet" type="text/css" href="css/main.css">
 	<link rel="stylesheet" type="text/css" href="css/nav/nav.css">
-=======
 	<link rel="stylesheet" type="text/css" href="css/main/main.css">
->>>>>>> d8ee541bc881ce8e689fe77199d561d3c74ba3f0
 	<title>Tour Top 100</title>
 </head>
 <body>
--- views/home.php
+++ views/home.php
@@ -1,6 +1,7 @@
 <div id="wrapper">
 
 <ul class="pagination">
+    <li>Juli</li>
     <li><a href="?page_nr=1">1</a></li>
     <li><a href="?page_nr=2">2</a></li>
     <li><a href="?page_nr=3">3</a></li>
