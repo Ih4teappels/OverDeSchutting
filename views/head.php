d8ee541bc881ce8e689fe77199d561d3c74ba3f0
Ih4teappels <kev10@live.nl>
2015-11-17 11:56:05 +0100

    update2.0

--- views/head.php
+++ views/head.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link rel="stylesheet" type="text/css" href="css/main/main.css">
 	<title>Tour Top 100</title>
 </head>
 <body>
