577994711fc3ee197f0b28748348e01ad292d754
Ih4teappels <kev10@live.nl>
2015-11-17 13:23:39 +0100

    updaterino

--- views/head.php
+++ views/head.php
@@ -1,10 +1,5 @@
 <html>
 <head>
-<<<<<<< HEAD
-
-	<link rel="stylesheet" type="text/css" href="css/main.css">
-=======
->>>>>>> 9a9ab979e3e368143ceeb8b712f2fa91ddb27849
 	<link rel="stylesheet" type="text/css" href="css/nav/nav.css">
 	<link rel="stylesheet" type="text/css" href="css/main/main.css">
 	<title>Tour Top 100</title>
