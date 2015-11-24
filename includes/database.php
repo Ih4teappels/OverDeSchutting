612ff436b779ae7bdb73ee1980ae15d0cadbf541
Ih4teappels <kev10@live.nl>
2015-11-24 11:45:55 +0100

    background update

--- includes/database.php
+++ includes/database.php
@@ -3,13 +3,13 @@
 //// De databaseverbinding
 //// Zie http://www.php.net/manual/en/book.mysqli.php
 //
-//$mysqli = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+$mysqli = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
 //
-//if ($mysqli->connect_errno) {
-//    echo "Failed to connect to MySQL:
-//    (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-//}
+if ($mysqli->connect_errno) {
+   echo "Failed to connect to MySQL:
+   (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
 //
-////echo $mysqli->host_info . "<hr>";
-//
+//echo $mysqli->host_info . "<hr>";
+// 
 //
--- index.php
+++ index.php
@@ -5,6 +5,10 @@ require 'includes/config.php' ;
 // Maak de database connectie
 require_once 'includes/database.php' ;
 
+
+include 'libs/functions.php';
+
+
 $page = (empty($_GET['page'])) ? '' : $_GET['page'];
 
 include 'views/head.php';
@@ -17,7 +21,21 @@ switch ($page) {
         include 'views/footer.php';
         break;
 
+    
+
+
+
     default:
+
+    // $page_nr = isset($_GET['page_nr']) ? $_GET['page_nr'] : 1;
+
+    // $videoItemId = isset($_GET['videoItemId']) ? $_GET['videoItemId'] : "";
+
+    // require 'models/videoitems.php';
+    
+    // $result = select_videoitems($page_nr);
+
+    
 
     include 'views/home.php';
 
