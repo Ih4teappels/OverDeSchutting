6851278d51dd114af82515da29fbf9b8c376b0a3
stijnana <stijn.olthof@live.nl>
2015-12-01 13:49:33 +0100

    luister button

--- views/header.php
+++ views/header.php
@@ -31,12 +31,14 @@
 
 <!--    <img src="img/logo.png" id="logo">-->
 
-  <div id="top">
+
 
 <!--    <img src="img/toptxt.png" id="toptxt" alt="">-->
 
     <div id="navi">
       <ul id="menu_wrap" class="Black"><li class="button"><a href="#">RADIO1.NL</a></li><li class="button"><a href="#">TOUR TOP 100</a></li> <li class="search"><a href="#"></a><input type="text" placeHolder="Search"  /></li></ul>
-    </div>
+        <a href="http://www.google.com" id="luister">
+        <img src="img/luister.png">
+        </a>
 
 </header>
