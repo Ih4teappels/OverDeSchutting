287a2556cc742ad2a4edac2342747b5412871e74
Ih4teappels <kev10@live.nl>
2015-12-01 12:03:27 +0100

    merge

--- views/home.php
+++ views/home.php
@@ -30,7 +30,7 @@
 
 <div id="pagWrapper">
 <?php
-            echo "<div id='month' class='monthFont'>".date("M")."</div>";
+            echo "<div id='month' class='monthFont'>"."JULI"."</div>";
             for($i = 1; $i <= 24; $i++){
                 echo "<div class='numberBlock'><p><b>".$i."</b></p></div>";
             }
