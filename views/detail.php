273d6b5451a4a9f38bbafe588b9dc15cc24ef6a2
Ih4teappels <kev10@live.nl>
2015-12-11 09:57:41 +0100

    update

--- views/detail.php
+++ views/detail.php
@@ -5,7 +5,7 @@
             echo  '<div id="list5"><div id="100List">';
             echo  '<h4>' . $results['song_title'] . '</h4><br>';
             echo  '<h3>' . $results['id'] . '</h3>';
-            echo  '<iframe width="420" height="315" src="' . $results['video'] . '?autoplay=1"></iframe>';
+            echo  '<iframe width="420" height="315" src="' . $results['video'] . '?autoplay=0"></iframe>';
             echo  '<p>' . $results['content'] . '</p><br>';
             echo  '</div></div>';
     
