346cb7d8be9e977abe8ddfdb00552cef934cfd09
stijnana <stijn.olthof@live.nl>
2015-12-15 13:58:11 +0100

    commit style

--- models/select_comments.php
+++ models/select_comments.php
@@ -6,24 +6,19 @@ $query2 = "SELECT * FROM reactionform WHERE song_id=$song ";
 // in query 2 zetten WHERE song_id = 2
 $result = $mysqli->query($query2);
 
-echo '<table style="width:25%">';
 
-echo '<tr>';
-echo '<th>' . 'name';
-echo '<th>' . 'email';
-echo '<th>' . 'website';
-echo '<th>' . 'reaction';
-echo '<th>' . 'created_at';
-echo '</tr>';
+
+
+
+
 
 while ($results = $result->fetch_assoc()){
 
-    echo '<tr>';
-    echo '<td>' . $results['name'] . '</td>';
-    echo '<td>' . $results['email'] . '</td>';
-    echo '<td>' . $results['website'] . '</td>';
-    echo '<td>' . $results['reaction'] . '</td>';
-    echo '<td>' . $results['created_at'] . '</td>';
+    echo $results['name'] . '</td>';
+    echo $results['email'] . '</td>';
+    echo $results['website'] . '</td>';
+    echo $results['reaction'] . '</td>';
+    echo $results['created_at'] . '</td>';
 
-    echo '</tr>';
+
 }
