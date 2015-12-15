765d98bd810eed540166e86fd81445788e96ba9b
Ih4teappels <kev10@live.nl>
2015-12-15 13:58:06 +0100

    added more links to homepage

--- views/viewsongs.php
+++ views/viewsongs.php
@@ -9,8 +9,10 @@ while ($results = $result->fetch_assoc()){
 	echo  '<div id="list5"><div id="100List">';
 	echo  '<a href="index.php?page=detail&song=' . $results['id'] . '"><img src="img/' . $results['thumbnail'] . '"></a>';
 	echo  '<h3 id="songId">' . $results['id'] . '</h3>';
-    echo  '<h4>' . $results['song_title'] . '</h4><br>';
-    echo  '<p>' . $results['small_content'] . '</p><br>';
+    echo  '<h4>' . $results['song_title'] . '</h4>';
+    echo  '<p>' . $results['small_content'] . '</p>';
+    echo '<a id="textText" href="index.php?page=detail&song=' . $results['id'] . '"><img src="img/text.png" id="textimg">Lees meer</a>';
+    echo '<img src="img/facebook.png" id="socialimg"><img src="img/twitter.png" id="socialimg"><img src="img/mail.png" id="socialimg">';
     // echo  $results['video'];
     echo  '</div></div>';
     
